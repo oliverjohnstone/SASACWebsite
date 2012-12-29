309f0a0d5277be3b602ae90e889dfa32e90645df
Oliver Johnstone <oliver.johnstone@clock.co.uk>
2012-12-29 16:22:25 +0000

    Moving some assets into a more appropriate location

--- assets/php/contactForm.php
+++ assets/php/contactForm.php
@@ -1,30 +1,30 @@
 <?php
-    $to = "oliver@ojs.co";
-    $responce = array(
-        "errors" => array()
-    );
+	$to = "oliver@ojs.co";
+	$responce = array(
+		"errors" => array()
+	);
 
-    if (!isset($_POST['forename'])) $responce["errors"][] = 'forename'; 
-    if (!isset($_POST['phone'])) $responce["errors"][] = 'phone'; 
+	if (!isset($_POST['forename'])) $responce["errors"][] = 'forename'; 
+	if (!isset($_POST['phone'])) $responce["errors"][] = 'phone'; 
 
-    $forename = $_POST['forename'];
-    $surname = $_POST['surname'] ? $_POST['surname']: "";
-    $email = $_POST['email'] ? $_POST['email']: "";
-    $phone = $_POST['phone'];
+	$forename = $_POST['forename'];
+	$surname = $_POST['surname'] ? $_POST['surname']: "";
+	$email = $_POST['email'] ? $_POST['email']: "";
+	$phone = $_POST['phone'];
 
-    if(count($responce["errors"]) > 0) {
-        die(json_encode($responce));
-    } else {
-        $responce["errors"] = false;
-    }
+	if(count($responce["errors"]) > 0) {
+		die(json_encode($responce));
+	} else {
+		$responce["errors"] = false;
+	}
 
-    $message = "Contact Request From: \n\nName: $forename $surname\nEmail: $email\nPhone: $phone";
-    $responcemsg = "Hi $forename $surname,\n\nThanks for contacting St Albans Sub Aqua Club. We will respond shortly.\n\nCheers,\nSt Albans Sub Aqua Club.";
+	$message = "Contact Request From: \n\nName: $forename $surname\nEmail: $email\nPhone: $phone";
+	$responcemsg = "Hi $forename $surname,\n\nThanks for contacting St Albans Sub Aqua Club. We will respond shortly.\n\nCheers,\nSt Albans Sub Aqua Club.";
 
-    mail($to, 'SASAC Contact Request', $message);
-    if (strlen($email) > 0) {
-        mail($email, "St Albans Sub Aqua Club", $responcemsg);
-    }
+	mail($to, 'SASAC Contact Request', $message);
+	if (strlen($email) > 0) {
+		mail($email, "St Albans Sub Aqua Club", $responcemsg);
+	}
 
-    die(json_encode($responce));
+	die(json_encode($responce));
 ?>
--- assets/php/forum.php
+++ assets/php/forum.php
@@ -1,43 +1,43 @@
 <?php
-    // define("DEBUG", 1);
-    define("APPLICATION_URL", "sasac.ojs.co");
+	// define("DEBUG", 1);
+	define("APPLICATION_URL", "sasac.ojs.co");
 
-    if (defined("DEBUG")) {
-        ini_set('display_errors',1); 
-        error_reporting(E_ALL);
-    }
- 
-    require_once("factory.php");
-    $auth = Factory::getAuth();
+	if (defined("DEBUG")) {
+		ini_set('display_errors',1); 
+		error_reporting(E_ALL);
+	}
 
-    $respone = new stdClass();
-    $email = null;
+	require_once("factory.php");
+	$auth = Factory::getAuth();
 
-    if (!$email = $auth->auth()) {
-        if (!isset($_POST["email"]) || !isset($_POST["password"])) {
-            $respone->errors = true;
-            echo json_encode($respone);
-            exit();
-        }
-        $email = $_POST["email"];
-        $password = $_POST["password"];
-        $remember = false;
-        if (isset($_POST["remember"]) && $_POST["remember"] == 1) $remember = true; 
-        if (!$email = $auth->auth($email, $password, $remember)) {
-            $respone->errors = true;
-            echo json_encode($respone);
-            exit();
-        }
-    }
-    header("Content-Type: application/json");
+	$respone = new stdClass();
+	$email = null;
 
-    $user = Factory::getUser($email);
-    if (!$user->isValid()) {
-        $respone->errors = true;
-    } else {
-        $contentGenerator = Factory::getContentGenerator($user);
-        $respone->body = $contentGenerator->buildContent();
-    }
-    
-    echo json_encode($respone);
+	if (!$email = $auth->auth()) {
+		if (!isset($_POST["email"]) || !isset($_POST["password"])) {
+			$respone->errors = true;
+			echo json_encode($respone);
+			exit();
+		}
+		$email = $_POST["email"];
+		$password = $_POST["password"];
+		$remember = false;
+		if (isset($_POST["remember"]) && $_POST["remember"] == 1) $remember = true; 
+		if (!$email = $auth->auth($email, $password, $remember)) {
+			$respone->errors = true;
+			echo json_encode($respone);
+			exit();
+		}
+	}
+	header("Content-Type: application/json");
+
+	$user = Factory::getUser($email);
+	if (!$user->isValid()) {
+		$respone->errors = true;
+	} else {
+		$contentGenerator = Factory::getContentGenerator($user);
+		$respone->body = $contentGenerator->buildContent();
+	}
+	
+	echo json_encode($respone);
 ?>
