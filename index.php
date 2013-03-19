c488894e79a045b2b253fc16b807290a5b3b1f25
Oliver Johnstone <oliver@ojs.co>
2013-03-19 22:17:52 +0000

    Adding google analitics code

--- index.php
+++ index.php
@@ -37,6 +37,17 @@ if ($mobileDetect->isMobile() && !$mobileDetect->isTablet()) {
 		<!--[if lte IE 6]>
 			<link rel="stylesheet" type="text/css" href="assets/css/ie6.css">
 		<![endif]-->
+		<script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-20496817-1']);
+			_gaq.push(['_trackPageview']);
+
+			(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+		</script>
 	</head>
 	<body>
 		<div class="fb-link"><a href="http://www.facebook.com/StAlbansSubAquaClub" target="_blank"></a></div>
